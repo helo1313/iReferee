924bdb01d15fb46f26c4ca89d4fd487c9c90a2ca
Helo <62700189+helo1313@users.noreply.github.com>
2022-08-30 22:41:19 +0200

    work on changing login/register screen to one view

--- iReferee/Controller/LoginViewController.swift
+++ iReferee/Controller/LoginViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("Loaded Login")
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+        authMetodChange(sender)
+        
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        authMetodChange(sender)
+    }
+    
+    @IBAction func confirmButtonPressed(_ sender: UIButton) {
+    }
+    
+    func authMetodChange(_ sender: UIButton){
+        signInButton.titleLabel?.textColor = UIColor(named: "App TextField")
+        signUpButton.titleLabel?.textColor = UIColor(named: "App TextField")
+        sender.titleLabel?.textColor = UIColor(named: "App ContrastGrey")
+        print(sender.titleLabel!)
     }
     
 
