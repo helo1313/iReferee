55ba4cf5a3a431fc5ee2cd85962bb58924fcf354
Helo <62700189+helo1313@users.noreply.github.com>
2022-09-06 15:15:59 +0200

    added register option

--- iReferee/Controller/RegisterViewController.swift
+++ iReferee/Controller/RegisterViewController.swift
@@ -6,16 +6,36 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func confirmButtonPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    print(e)
+                } else {
+                    // Register successful
+                    self.performSegue(withIdentifier: "registerSuccessful", sender: self)
+                }
+                
+            }
+        }
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
